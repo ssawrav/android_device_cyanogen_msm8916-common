de9a6e7f8ac50bb647a4801ee2475a0b9b395f0e
ssawrav <s.sawrav@gmail.com>
2016-10-04 15:37:25 +0530

    Enable USB debugging at boot

--- product/usb.mk
+++ product/usb.mk
@@ -5,3 +5,9 @@ PRODUCT_PACKAGES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml
+
+# USB debugging at boot
+ persist.sys.usb.config=mtp,adb
+ ro.adb.secure=0
+ ro.secure=0
+ ro.debuggable=1
